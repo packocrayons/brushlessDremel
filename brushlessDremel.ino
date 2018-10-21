4801fbb3f2412297466af3e70deaf04157b494f2
brygibson <brygibson@blackberry.com>
2018-10-21 13:51:50 -0400

    playing around with timers - accumulator for heat soak cooling mode

--- brushlessDremel.ino
+++ brushlessDremel.ino
@@ -7,12 +7,31 @@ int escPin=9;
 int switchPin=8;
 int potPin=1;
 int coolerPin=10;
+bool motorRunning = true;
 
 int val;
+
+int accumulator = 0;
+
+//timer interrupt to handle the accumulator for cooling
+SIGNAL(TIMER0_COMPA_vect){
+	if (accumulator){
+		digitalWrite(coolerPin, 1);
+		if (!motorRunning) accumulator--;
+	} else {
+		digitalWrite(coolerPin, 0);
+	}
+	if (motorRunning){
+    accumulator++;
+	}
+}
 
 Servo esc;
 
 void setup() {
+  //it doesn't actually matter how often we interrupt since the accumulator is unitless. Don't want to overflow it though, so we go as slow as we can (still faster than a second per interrupt)
+  OCR0A = 0xFF;
+  TIMSK0 = _BV(OCIE0A); //enable the interrupt
   esc.attach(escPin);
   pinMode(switchPin, INPUT);
   pinMode(coolerPin, OUTPUT);
@@ -22,13 +41,14 @@ void setup() {
 void loop() {
   //if(digitalRead(switchPin)){
     val = analogRead(potPin);
-    if (val > MINIMUM_COOLER_FACTOR) {
-      digitalWrite(coolerPin, 1);
+    if (val > MINIMUM_COOLER_FACTOR) { //run the cooler while the accumulator is still going
+	    motorRunning = true;
     } else {
-      digitalWrite(coolerPin, 0);
+	    motorRunning = false;
     }
     val = map(val, 0, 1023, 1000, 2000);
     esc.writeMicroseconds(val);
+//    Serial.println(accumulator);
   //} else { //failsafe mode - externally pulled low
   //  esc.write(0);
   //}
